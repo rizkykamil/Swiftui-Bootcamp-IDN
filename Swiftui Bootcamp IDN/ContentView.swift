17537437b0381522eba782be93abd32377f47e4c
rizkykamil.dll <rabbaniikamil@gmail.com>
2021-09-27 08:24:01 +0700

    Initial Commit

---
+++ Swiftui Bootcamp IDN/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swiftui Bootcamp IDN
+//
+//  Created by Rizky Kamil Rabbani on 27/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Swiftui Bootcamp IDN/Swiftui_Bootcamp_IDNApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Bootcamp_IDNApp.swift
+//  Swiftui Bootcamp IDN
+//
+//  Created by Rizky Kamil Rabbani on 27/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Bootcamp_IDNApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
